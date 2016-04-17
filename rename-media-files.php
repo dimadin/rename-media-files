6fd0c6f899f70e70c0af44e3e01e330d004b87a8
Milan Dinić <forumi@srpski.biz>
2016-04-17 11:55:40 +0200

    Add link to services page in plugin actions links.

--- rename-media-files.php
+++ rename-media-files.php
@@ -76,7 +76,9 @@ function rename_media_files_filter_plugin_actions( $links, $file ) {
 
 	if ( $file == $this_plugin ) {
 		$donate_link = '<a href="http://blog.milandinic.com/donate/">' . __( 'Donate', 'rename-media-files' ) . '</a>';
-		$links = array_merge( array( $donate_link ), $links ); // Before other links
+		$wpdev_link  = '<a href="http://blog.milandinic.com/wordpress/custom-development/">' . __( 'WordPress Developer', 'rename-media-files' ) . '</a>';
+
+		$links = array_merge( array( $donate_link, $wpdev_link ), $links ); // Before other links
 	}
 
 	return $links;
